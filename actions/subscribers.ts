24100888e737fdc1fb7ff8a0d3233c80f0678e90
shaagar <shaagar5@gmail.com>
2026-05-10 18:29:25 +0545

    newsletter form added

--- actions/subscribers.ts
+++ actions/subscribers.ts
@@ -4,14 +4,19 @@ import dbConnect from "@/lib/mongoose"
 import Subscribers from "@/models/subscribers";
 import { revalidatePath } from "next/cache";
 
-// CREATE Subscriber
-export default async function createSubscriber(email: string) {
+// CREATE Subscriber for newsletter
+export async function createSubscriber(email: string) {
     try {
         await dbConnect()
 
+        const existingSubscriber = await Subscribers.findOne({ email })
+
+        if (existingSubscriber) {
+            return { success: true, error: false, msg: "You're already subsribed" };
+        }
+
         await Subscribers.create({ email })
         revalidatePath("/dashboard/subscribers")
-
         return { success: true, error: false };
     } catch (error: any) {
         return { success: false, error: error.message || "Failed | Create Subscriber" };
--- components/app/newsletter-form.tsx
+++ components/app/newsletter-form.tsx
@@ -1,13 +1,56 @@
+"use client"
+
+import { useForm } from "react-hook-form"
+import { z } from "zod"
+import { zodResolver } from "@hookform/resolvers/zod"
 import { Send } from "lucide-react"
+import { createSubscriber } from "@/actions/subscribers"
+import { toast } from "sonner"
+
+const NewsLetterFormSchema = z.object({
+    email: z.email({
+        error: "Pls Enter valid Email"
+    })
+})
+
+type NewsLetterFormData = z.infer<typeof NewsLetterFormSchema>
 
 export default function NewsletterForm() {
 
+    const { register, reset, handleSubmit, formState: { isLoading, errors } } = useForm<NewsLetterFormData>({
+        resolver: zodResolver(NewsLetterFormSchema as any)
+    })
+
+    const submitHandler = async (data: NewsLetterFormData) => {
+        try {
+            const res = await createSubscriber(data.email)
+
+            if (res.msg) {
+                toast.success(`✅ ${res.msg}`)
+                reset()
+                return
+            }
+
+            toast.success("✅ Subscribing to newsletter successful")
+            reset()
+        } catch (error: any) {
+            toast.error("❌ Subscribing to newsletter failed")
+        }
+    }
+
     return (
         <div className="relative text-black">
-            <form action="">
-                <input className="ps-5 pe-10 py-3 bg-brand-2 rounded-sm" type="email" placeholder='Enter your email address' />
-                <Send className="size-5 absolute right-4 top-3.5" />
+            <form onSubmit={handleSubmit(submitHandler)}>
+                <div>
+                    <input {...register("email")} className="ps-5 pe-10 py-3 bg-brand-2 rounded-sm" type="email" placeholder='Enter your email address' />
+                    <button className="cursor-pointer" type="submit">
+                        <Send className="size-5 absolute right-4 top-3.5" />
+                    </button>
+                </div>
+                {errors.email && (
+                    <p className="text-red-400 text-sm px-4 py-1">{errors.email.message}</p>
+                )}
             </form>
-        </div>
+        </div >
     )
 }
